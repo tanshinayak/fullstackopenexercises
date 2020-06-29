fe7dc8aa8453e5e6a55814105c3e8ae9053b2b41
Tanshi Nayak <tanshitn@gmail.com>
2020-06-29 23:47:29 +0530

    Add Findmaxanecdote function

--- part1/anecdote/src/index.js
+++ part1/anecdote/src/index.js
@@ -11,6 +11,7 @@ const App=()=>{
     'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
   ]
   const [num,setnum]=useState(0)
+  const [maxnum,setmaxnum]=useState(0)
   const [vote,setvote]=useState([0,0,0,0,0,0])
   const randomquote=()=>{
    setnum(Math.floor(Math.random() * anecdotes.length))
@@ -19,13 +20,18 @@ const App=()=>{
     const copy=[...vote]
     copy[n]+=1
     setvote(copy)
+    setmaxnum(vote.indexOf(Math.max(...vote)))
   }
   return(
     <div>
+      <h1>Anecdote of the Day</h1>
     <p>{anecdotes[num]}</p>
     <p>Has {vote[num]} votes</p>
     <button onClick={()=>votequote(num)}>Vote</button>
     <button onClick={randomquote}>Next</button>
+    <h1>Anecdote with most number of votes</h1>
+    <p>{anecdotes[maxnum]}</p>
+    <p>Has {vote[maxnum]} votes</p>
     </div>
   )
 }
